23265277e8d26e9576c8d9d5fbba0e67817c4789
haseeb-ahmad <engg.haseeb@gmail.com>
2013-12-25 05:12:15 +0500

    Confirmation Email Feature Modified

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,15 +1,20 @@
 class RegistrationsController < Devise::RegistrationsController
 	
-    def create
-        user = User.where("email = '#{sign_up_params[:email]}'").first
-        
-        if user
-            if user.encrypted_password.blank?
-        	   user.send_confirmation_instructions
-            end
-            redirect_to root_path
-        else
-            super
-        end
+	def create
+        # Ckeck if user exists
+		user = User.where("email = '#{sign_up_params[:email]}'").first
+		
+		if user
+            # If user is present without password then send emial for confirmation
+			if user.encrypted_password.blank?
+				user.confirmed_at = ""
+                user.save!
+
+				user.send_confirmation_instructions
+			end
+			redirect_to root_path
+		else
+			super
+		end
 	end
 end
