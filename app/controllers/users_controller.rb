ea649a1b1a5d608046f90b4b3bd4a5562d75ab82
Haseeb Ahmad <engg.haseeb@gmail.com>
2013-12-27 02:06:14 +0500

    before filter applied for authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,35 +1,22 @@
 class UsersController < ApplicationController
+before_filter :authenticate_user!
 
 	def index
-		if current_user
-			# Signed In User
-			@user = current_user
-			@identities = current_user.identities
-		else
-			# User needs to sign up / sign in to continue
-			redirect_to new_user_registration_path
-		end
+    @user = current_user
+    @identities = current_user.identities
 	end
 
 	def contact
-		if current_user
-			# Signed In User
-			@user = current_user
-			@identities = current_user.identities
-			@friends = @user.get_friends
-
-		else
-			# User needs to sign up / sign in to continue
-			redirect_to new_user_registration_path
-		end
+    @user = current_user
+    @identities = current_user.identities
+    @friends = @user.get_friends
 	end
 
 	def disconnect
-		identity = Identity.where("user_id = '#{current_user.id}' AND provider = '#{params[:identity]}'").first
-		identity.delete
-
-		flash.now[:alert] = "You are disconnected from #{identity.provider.capitalize}."
-		redirect_to users_index_path
+    identity = Identity.where("user_id = '#{current_user.id}' AND provider = '#{params[:identity]}'").first
+    identity.delete
+    flash.now[:alert] = "You are disconnected from #{identity.provider.capitalize}."
+    redirect_to users_index_path
 	end
 
 end
