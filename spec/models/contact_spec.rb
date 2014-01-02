36c4a3363e44fd4baf77efc967bfdce3152ed475
haseeb-ahmad <engg.haseeb@gmail.com>
2014-01-02 18:46:01 +0500

    Contact Notes Added, Contact's Features Modified

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,14 +1,17 @@
 require 'spec_helper'
 
 describe Contact do
+	let(:contact) { FactoryGirl.create(:contact) }
+
 	describe 'Associations' do
 		it { should belong_to :user }
 		it { should belong_to :identity }
 	end
 
 	context "validations" do
-		it "should require a name" do
-			
+		it "should require a full name" do
+			contact.full_name = ""
+			contact.should_not be_valid
 		end
 	end
 end
