f240d69a25c9116f429de8636ebc3bc1fb8397be
t-aldaim <talha.wasim@aldaimsolutions.com>
2018-02-20 17:59:53 +0500

    upgrading rails version to fix rails specific bugs

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,38 +2,38 @@ class RegistrationsController < Devise::RegistrationsController
 	
 	def update
 	  @user = current_user
-	  if params[:user][:password].blank?
-	  	@user.first_name = params[:user][:first_name]
-	  	@user.last_name = params[:user][:last_name]
-	  	send_confirmation = false
-	  	
-	  	if (@user.email != params[:user][:email])
-	  		send_confirmation = true
-	  		@user.unconfirmed_email = params[:user][:email]
-		  	@user.confirmation_token = Devise.friendly_token
-		  	@user.confirmation_sent_at = Time.now.utc
-	  	end
-	  	@user.save
-	  	@user.send_confirmation_instructions if send_confirmation
-	  	flash[:notice] = "Profile updated successfully!"
-	    redirect_to :back
-	  else
-	  	@user.send(:update_with_password, params[:user])
-	  	flash[:notice] = "Profile updated successfully!"
-	    redirect_to :back
-	  end
-	  # method = if params[:user][:password].blank?
-	  #            :update_without_password
-	  #          else
-	  #            :update_with_password
-	  #          end
+	  # if params[:user][:password].blank?
+	  # 	@user.first_name = params[:user][:first_name]
+	  # 	@user.last_name = params[:user][:last_name]
+	  # 	send_confirmation = false
 
-	  # if @user.send(method, params[:user])
-	  #   flash[:notice] = "Profile updated successfully!"
+	  # 	if (@user.email != params[:user][:email])
+	  # 		send_confirmation = true
+	  # 		@user.unconfirmed_email = params[:user][:email]
+		 #  	@user.confirmation_token = Devise.friendly_token
+		 #  	@user.confirmation_sent_at = Time.now.utc
+	  # 	end
+	  # 	@user.save
+	  # 	@user.send_confirmation_instructions if send_confirmation
+	  # 	flash[:notice] = "Profile updated successfully!"
 	  #   redirect_to :back
 	  # else
-	  #   render :edit
+	  # 	@user.send(:update_with_password, params[:user])
+	  # 	flash[:notice] = "Profile updated successfully!"
+	  #   redirect_to :back
 	  # end
+	  method = if params[:user][:password].blank?
+	             :update_without_password
+	           else
+	             :update_with_password
+	           end
+
+	  if @user.send(method, params[:user])
+	    flash[:notice] = "Profile updated successfully!"
+	    redirect_to :back
+	  else
+	    render :edit
+	  end
 	end
 
 	def create
@@ -63,27 +63,30 @@ class RegistrationsController < Devise::RegistrationsController
 					redirect_to new_user_session_path
 				end
 			else
-				build_resource
-				resource.email = params[:user][:email]   #******** here resource is user 
-				resource.confirmation_token = Devise.friendly_token
-				resource.confirmation_sent_at = Time.now.utc
+				# build_resource
+				# resource.email = params[:user][:email]   #******** here resource is user 
+				# resource.confirmation_token = Devise.friendly_token
+				# resource.confirmation_sent_at = Time.now.utc
 
-				if resource.save!(:validate => false)
-				  if resource.active_for_authentication?
-				    set_flash_message :notice, :signed_up if is_navigational_format?
-				    sign_in(resource_name, resource)
-				    respond_with resource, :location => after_sign_up_path_for(resource)
-				  else
-				    set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
-				    expire_session_data_after_sign_in!
-				    respond_with resource, :location => after_inactive_sign_up_path_for(resource)
-				  end
-				else
-				  clean_up_passwords resource
-				  respond_with resource
-				end
+				# if resource.save!(:validate => false)
+				#   if resource.active_for_authentication?
+				#     set_flash_message :notice, :signed_up if is_navigational_format?
+				#     sign_in(resource_name, resource)
+				#     respond_with resource, :location => after_sign_up_path_for(resource)
+				#   else
+				#     set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
+				#     expire_session_data_after_sign_in!
+				#     respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+				#   end
+				# else
+				#   clean_up_passwords resource
+				#   respond_with resource
+				# end
+
+				# if signup using just email
 				# New User, add user to database
 				# send confirmation email.
+				super
 			end
 		end
 	end
