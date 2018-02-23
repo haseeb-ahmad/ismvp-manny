60af907f30792ec4f954a8e6da961e9754d0ea26
Haseeb Ahmad <engg.haseeb@gmail.com>
2018-02-23 20:57:06 +0500

    Error solved.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
 	before_filter :configure_devise_params, if: :devise_controller?
 	layout :dynamic_layout
-	before_filter :authenticate_user!
 
 	def after_sign_up_path_for(resource)
 		after_sign_in_path_for(resource)
