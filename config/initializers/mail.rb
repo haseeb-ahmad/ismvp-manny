5cd8426af270e8456a5c20097622c8aec7f82aae
Haseeb Ahmad <engg.haseeb@gmail.com>
2018-02-23 20:59:32 +0500

    email issue.

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
 	:address				=> "smtp.gmail.com",
 	:port					=> 587,
 	:domain					=> "gmail.com",
-	:user_name				=> ENV['SMTP_USERNAME'],
-	:password				=> ENV['SMTP_PASSWORD'],
+	:user_name				=> "paksols@gmail.com",
+	:password				=> "123jkl123jkl",
 	:authentication			=> "plain",
 	:enable_starttls_auto	=> true
 }
